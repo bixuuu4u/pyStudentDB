e6cf49d35c11fcda9630163f7a9c7af3684fdf98
bixuuu4u <biswajeetpc110@outlook.com>
2025-04-19 13:50:02 +0530

    Print Formatted

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Rea
 # Write
 #FILENAME
-FILENAME ="pyStudentDB\data.txt"
+FILENAME ="pyStudentDB\\data.txt"
 class Student:
     def __init__(self, name, roll, marks, grade):
         self.name = name
@@ -35,31 +35,34 @@ def write():
 def read():
     try:
         with open(FILENAME, 'r') as fr:
-            print("\nStudent Records from File:\n")
-            i=1
-            for line in fr:
-              data=line.strip().split()
-              sroll=data[-8]
-              smarks=data[-7:-1]
-              sgrade=data[-1]
-              sname = " ".join(data[:-8])
-
-              smarks = " ".join(smarks)
-              # print(data,type(data))
-            #   print("-----------------------------------------------------")
-            #   print(f"SL NAME       ROLL PHY CHEM MATH IT ENG HINDI  GRADE")
-            #   print(f"{i} {sname}           {sroll} {smarks} {sgrade}")
-
-              print(f"SL:{i} NAME:{sname} ROLL:{sroll} MARKS:{smarks} GRADE:{sgrade}")
-              i+=1
+           lines = fr.readlines()
+           display(lines)
     except FileNotFoundError:
         print("File not found!")
+
+def display(lines):
+    print("\nStudent Records in Table Format:\n")
+    print("+----+------------------------------+--------+-------------------------------------------+--------+")
+    print("| SL | NAME                         | ROLL   |  PHY    CHEM   MATH    IT    ENG   HINDI  | GRADE  |")
+    print("+----+------------------------------+--------+-------------------------------------------+--------+")
+    sl = 1
+    for line in lines:
+        data = line.strip().split()
+        if len(data) < 9:
+            continue
+        name = " ".join(data[:-8])
+        roll = data[-8]
+        marks = data[-7:-1]
+        grade = data[-1]
+        formatted_marks = "  ".join(f"{float(mark):>5.1f}" for mark in marks)
+        print(f"| {sl:<2} | {name:<28} | {roll:<6} | {formatted_marks:<10}  | {grade:^6} |")
+        sl += 1
+    print("+----+------------------------------+--------+-------------------------------------------+--------+")
+
 
 def main():
     # write()
     read()
-def display():
-    print("--------------------------------------------")
 
 
 
